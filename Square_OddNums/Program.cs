fe3c538d35d537a3837108f25edb583ce0e18815
Ellety <thenarets@gmail.com>
2025-11-21 18:30:11 +0300

    0

---
+++ Square_OddNums/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Channels;
+
+namespace Square_OddNums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+
+            int n = Int32.Parse(Console.ReadLine());
+
+            List<int> oddNumbers = GetOddNumbers(n);
+
+            Console.WriteLine(string.Join(", ", oddNumbers));
+
+            Console.WriteLine(GetString(n));
+        }
+        static List<int> GetOddNumbers(int n)
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 != 0)
+                    numbers.Add(i);
+            }
+
+            return numbers;
+        }
+
+        static string GetString(int n)
+        {
+            string str = "";
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    str += "X";
+                }
+
+                str += "\n";
+            }
+
+            return str;
+        }
+    }
+}
